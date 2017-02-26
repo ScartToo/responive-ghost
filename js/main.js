962bfe447db01c26956e8244f1cacfcb3ede4b6d
ScartToo <1129970100@qq.com>
2017-02-26 21:49:58 +0800

    responsive-ghost
    基于bootstrap的响应式ghost

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2017/2/7 0007.
+ */
+$(function () {
+    var scrollHeight = $(window).height();
+    console.log(scrollHeight);
+    console.log(window.screen.height);
+});
